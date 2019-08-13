e7fe6d1cd0fc41e0a955521b2be32eb09cc139fc
credits12 <54032201+credits12@users.noreply.github.com>
2019-08-13 15:03:47 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("610460106433232933")
+setInterval(function() {
+channel.send(`عطني يالبوت كريديت كثار مره مره مره مره عطني كثييير مرررررررره
+`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
